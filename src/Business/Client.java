5c8962e86625e914c7216a3598457be840161218
Deydy Rodrigues <deydyrodrigues868@gmail.com>
2026-01-15 02:04:09 -0300

    Últimas modificações no conversor de moedas

--- src/Business/Client.java
+++ src/Business/Client.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+package Business;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 public class Client {
+
+    public static double Resposta(String moedaBase, double valorBase, String moedaConvertida)
+            throws IOException, InterruptedException {
+
+       moedaBase=moedaBase.toUpperCase();
+       moedaConvertida = moedaConvertida.toUpperCase();
+
+       String API_KEY = "d4e4877ffb187f17e805685c";
+
+       String url = "https://v6.exchangerate-api.com/v6/"
+               + API_KEY + "/latest/" + moedaBase;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        Gson gson = new Gson();
+        JsonObject json = gson.fromJson(response.body(),JsonObject.class);
+
+        double taxa = json.getAsJsonObject("conversion_rates").get(moedaConvertida).getAsDouble();
+        return valorBase * taxa;
+    }
 }
--- src/Business/Main.java
+++ src/Business/Main.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+package Business;
 
-public class Main {
-}
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+    public class Main{
+        public static void main(String[] args)throws IOException, InterruptedException {
+
+            Scanner scanner = new Scanner(System.in);
+
+
+            try {
+                System.out.println("""
+                Bem vindo ao menu de conversão
+                Tecle 1 para escolher uma moeda
+                Tecle qualquer outro numero para sair
+                """);
+
+                int scannerEntrada = scanner.nextInt();
+                scanner.nextLine();
+
+                while (scannerEntrada == 1) {
+
+                    System.out.println("Selecione a moeda base:");
+                    String moedaBase = scanner.nextLine();
+
+                    System.out.println("Selecione o valor desejado:");
+                    double valor = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.println("Selecione a moeda para conversão:");
+                    String moedaConvert = scanner.nextLine();
+
+                    System.out.println(
+                            Client.Resposta(moedaBase, valor, moedaConvert)
+                    );
+
+                    System.out.println(
+                            "\nDigite 1 para fazer outra conversão ou qualquer outro número para sair:"
+                    );
+                    scannerEntrada = scanner.nextInt();
+                    scanner.nextLine();
+                }
+
+                System.out.println("Programa encerrado.");
+
+            } catch (InputMismatchException e) {
+                System.out.println(" Erro: você digitou texto onde era esperado um número.");
+            } catch (NullPointerException e) {
+                System.out.println(" Erro: campo obrigatório vazio ou nulo.");
+            } finally {
+                scanner.close();
+            }
+        }
+
+    }
+
